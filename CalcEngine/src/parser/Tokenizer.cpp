b630dc2d5a1ea827eed4e51646fe337a1edd2c84
Mubashir Ehsan <mubashirehsan10@gmail.com>
2026-06-14 17:33:28 +0500

    designing tokenizer

---
+++ CalcEngine/src/parser/Tokenizer.cpp
@@ -0,0 +1,19 @@
+#include "Tokenizer.h"
+
+Token::Token(TokenType type, string value)
+{
+
+
+
+}
+
+
+Tokenizer::Tokenizer(string input)
+{
+	pos = 0;
+	this->input = input;
+}
+vector<Token> Tokenizer::tokenize()
+{
+	
+}
--- CalcEngine/src/parser/Tokenizer.h
+++ CalcEngine/src/parser/Tokenizer.h
@@ -1,1 +1,31 @@
 #pragma once
+#include <string>
+#include <vector>
+using namespace std;
+
+enum class TokenType {
+    NUMBER,
+    VARIABLE,
+    OPERATOR,
+    FUNCTION,
+    LPAREN,
+    RPAREN
+};
+
+class Token {
+public:
+    TokenType type;
+    string value;
+
+    Token(TokenType type, string value);
+};
+
+class Tokenizer {
+private:
+    string input;
+    int pos;
+
+public:
+    Tokenizer(string input);
+    vector<Token> tokenize();
+};
